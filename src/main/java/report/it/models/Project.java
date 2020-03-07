4a306914c5ececfdbf9d6ca105dbd0bbaba8592d
lth-li <zhuli.kina@gmail.com>
2020-03-07 19:24:22 +0100

    add an attribute and extends constructor

--- src/main/java/report/it/models/Project.java
+++ src/main/java/report/it/models/Project.java
@@ -10,13 +10,16 @@ public class Project {
     }
 
     public Project(String name) {
-
         this.name = name;
       //  this.administrator = administrator;
     }
 
     public Project(String name, String administrator){
         this.name = name;
+        this.administrator = administrator;
+    }
+
+    public void setAdministrator(String administrator){
         this.administrator = administrator;
     }
     public String getName() {
