d6b08bc093829361208828b16a72b04eed314946
ehsan <ehsanollah15@hotmail.com>
2020-03-08 00:47:17 +0100

    Have implemented the methods doGet and changed the implementation of methods removeUserFromProject and addMemberToProject. Implemented a little code in modify-project.jsp and added two attributes and methods in ProjectMember.

--- src/main/java/report/it/GroupModifier.java
+++ src/main/java/report/it/GroupModifier.java
@@ -3,8 +3,11 @@ package report.it;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /*
@@ -13,19 +16,36 @@ This class is for modifying project groups.
 public class GroupModifier extends ServletBase {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+        PrintWriter out = response.getWriter();
+        out.println(getPageIntro());
 
+        String currentUsername = "";
+
+        HttpSession session = request.getSession(true);
+        Object nameObj = session.getAttribute("username");
+        if (nameObj != null)
+            currentUsername = (String) nameObj;  // if the name exists typecast the name to a string
+            request.setAttribute("user",currentUsername);
+
+        // check that the user is logged in
+        if (!loggedIn(request)) {
+            response.sendRedirect("LogIn");
+        } else {
+            request.getRequestDispatcher("GroupModifier.jsp").include(request, response);
+
+        }
+    }
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
     }
 
     public boolean addUserToGroup(String username, String project) {
         boolean added = true;
         try {
-            String query = "INSERT INTO ProjectMembers WHWRE username = ? and  project_name=?";
+            String query = "SELECT * FROM projectMembers WHWRE username = ? and  project_name=?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, project);
@@ -64,7 +84,7 @@ public class GroupModifier extends ServletBase {
     public boolean addMemberToProject(String username, String project, String role) {
         boolean added = true;
         try {
-            String query = "INSERT INTO ProjectMembers WHWRE username = ? and  project_name=? and role=?";
+            String query = "SELECT * FROM ProjectMembers WHWRE username = ? and  project_name=? and role=?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, project);
@@ -119,4 +139,20 @@ public class GroupModifier extends ServletBase {
 
         return changed;
     }
+    private String getFullName(String username) {
+        String fullName = "";
+        try {
+        PreparedStatement statement = connection.prepareStatement("select * from Administrators where username = ?");
+        statement.setString(1, username);
+        ResultSet set = statement.executeQuery();
+
+        while (set.next()) {
+            fullName = set.getString("name");
+
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+        return fullName;
+}
 }
--- src/main/webapp/modify-project.jsp
+++ src/main/webapp/modify-project.jsp
@@ -1,3 +1,4 @@
+<%@ page import="report.it.models.Administrator" %>
 <%--
   Created by IntelliJ IDEA.
   User: abdusalamyabrak
@@ -6,11 +7,51 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
+<h2 style="color: aquamarine">Members</h2>
 
-</body>
-</html>
+<table>
+    <tr>
+        <th>Username</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+    </tr>
+
+    <c:forEach items="${ProjectMember}" var="Member">
+        <tr>
+             <td>
+                ${Member.getUsername()}
+            </td>
+            <td>
+                 ${Member.getProjectName()}
+            </td>
+            <td>
+                 ${Member.getEmail()}
+             </td>
+             <td>
+                  ${Member.getRole()}
+             </td>
+
+                 <td>
+                     <form action="GroupModifier" method="post">
+                         --
+                         --
+                        <button>Role</button>
+                     </form>
+            </td>
+        </tr>
+    </c:forEach>
+</table>
+<br>
+
+
+<h2 style="color: aquamarine">New Member</h2>
+
+
+<form action="GroupModifier" method="post">
+    <input type="text" name="member" placeholder="Member Name"/>
+    <input type="text" name="role" placeholder="Role"/>
+
+
+</form>
+<br>
