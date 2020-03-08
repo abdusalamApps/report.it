6ee751328e4421c564558c90ee8b7fa2f638e83c
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-03-09 00:35:07 +0100

    Update GroupModifier.java

--- src/main/java/report/it/GroupModifier.java
+++ src/main/java/report/it/GroupModifier.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /*
 This class is for modifying project groups.
