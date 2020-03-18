53c6fbc4d1e010231af92dd0e0526eb59649a64c
lth-li <zhuli.kina@gmail.com>
2020-03-18 20:01:42 +0100

    fyll table with usersfullname instead of username

--- src/main/java/report/it/ServletBase.java
+++ src/main/java/report/it/ServletBase.java
@@ -95,6 +95,22 @@ public class ServletBase extends HttpServlet {
         }
         return fullName;
     }
+    protected String getUserFullName(String username) {
+        String fullName = "";
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from Users where username = ?");
+            statement.setString(1, username);
+            ResultSet set = statement.executeQuery();
+
+            while (set.next()) {
+                fullName = set.getString("name");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return fullName;
+    }
 
 
     protected String encryptPassword(String password) {
