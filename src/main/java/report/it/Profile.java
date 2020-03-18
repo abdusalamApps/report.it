a754dfe66005c005532c02472ecfadf656431aa8
abdo <abdelsalem.yabrag@hotmail.com>
2020-03-18 18:49:23 +0100

    Added methods to changing user password

--- src/main/java/report/it/Profile.java
+++ src/main/java/report/it/Profile.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.PreparedStatement;
@@ -19,6 +20,8 @@ import java.util.List;
 @WebServlet("/Profile")
 public class Profile extends ServletBase {
 
+    private String currentUsername = "";
+
     public Profile() {
         super();
         // TODO Auto-generated constructor stub
@@ -27,11 +30,10 @@ public class Profile extends ServletBase {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter out = response.getWriter();
 
-        String currentUsername = "";
 
         HttpSession session = request.getSession(true);
         Object nameObj = session.getAttribute("username");
-        if (nameObj != null){
+        if (nameObj != null) {
             currentUsername = (String) nameObj;
         }
 
@@ -52,17 +54,18 @@ public class Profile extends ServletBase {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        switch (request.getParameter("action")){
+        switch (request.getParameter("action")) {
             case "changePassword":
                 System.out.println("action changePassword");
-                String oldPassword= request.getParameter("password");
-                String newPassword1= request.getParameter("newPassword1");
-                String newPassword2= request.getParameter("newPassword2");
-                String username= request.getParameter("username");
-                try {
-                    request.setAttribute("yourPassword", changePassword(oldPassword,newPassword1,newPassword2, username));
-                } catch (SQLException e) {
-                    e.printStackTrace();
+
+                String currentPassword = request.getParameter("password");
+                String newPassword1 = request.getParameter("newPassword1");
+                String newPassword2 = request.getParameter("newPassword2");
+
+                if (checkCurrentPassword(currentPassword)) {
+                    request.setAttribute("newPasswordMessage", changePassword(newPassword1, newPassword2));
+                } else {
+                    request.setAttribute("newPasswordMessage" , "The password you entered is incorrect!");
                 }
 
                 break;
@@ -73,35 +76,45 @@ public class Profile extends ServletBase {
         doGet(request, response);
     }
 
-    private boolean checkCurrentPassword (String currentPassword){
+    private boolean checkCurrentPassword(String currentPassword) {
+        String rightPassword = "";
 
-        return false;
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(
+                            "select password from Users where username = ?");
+            preparedStatement.setString(1, currentUsername);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                rightPassword = resultSet.getString("password");
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+        return rightPassword.equals(encryptPassword(currentPassword));
     }
 
-    private String changePassword(String oldPassword, String newPassword1, String newPassword2, String username) throws SQLException {
-     if (!newPassword1.equals(newPassword2)){
-         return "newPassword1 and newPassword2 don't mach each other";
-     }
-     else{
-         try {
-         String query = "select password from Users where username="+username;
-         PreparedStatement preparedStatement = connection.prepareStatement(query);
-         ResultSet rs = preparedStatement.executeQuery();
-             while (rs.next()) {
-                if(oldPassword.equals(rs.getString("password"))){
-                    String sql= "update Users set password= "+ newPassword1+" where username="+username;
-                    PreparedStatement stm = connection.prepareStatement(sql);
-                    stm.executeUpdate();
-                    return "your Password changed successfuly ";
-                }
-             }
-             preparedStatement.close();
-         } catch (SQLException ex) {
-             System.out.println("SQLException: " + ex.getMessage());
-             System.out.println("SQLState: " + ex.getSQLState());
-             System.out.println("VendorError: " + ex.getErrorCode());
-         }
-         return "your Password is wrong ";
-     }
+    private String changePassword(String newPassword1, String newPassword2) {
+        if (!newPassword1.equals(newPassword2)) {
+            return "The new password doesn't match";
+        } else {
+            try {
+                PreparedStatement stm =
+                        connection.prepareStatement("update Users set password = ? where username = ?");
+                stm.setString(1, encryptPassword(newPassword1));
+                stm.setString(2, currentUsername);
+                stm.executeUpdate();
+                return "Your new password is " + newPassword1;
+
+            } catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+            }
+            return "Something went wrong!";
+        }
     }
 }
